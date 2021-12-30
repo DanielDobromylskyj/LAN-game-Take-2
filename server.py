b5dd2623e6374797b70285081dfb7c640df0b3fc
DanielDobromylskyj <90842024+DanielDobromylskyj@users.noreply.github.com>
2021-12-30 21:10:28 +0000

    Now binds and starts running game
    No multiplayer yet

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 import socket
 import threading
 
-HOST = socket.gethostname()
+HOST = socket.gethostbyname(socket.gethostname())
 PORT = 65432
 print("Host: ", HOST)
 print("Port: ", PORT)
 
 players = []
+
 
 #x = threading.Thread(target=NewPlayer, args=(addr,))
 #x.start()
@@ -30,25 +31,40 @@ def testrig(conn,addr):
                         print("player connected")
 
 
+
                 else:
                     conn.sendall(b'Failed To Connect Game Full') # not working
+
+
             else:
                 if data.startswith(b'd'):
                     #player_data =  "<" + (data.split(b"_")) + "..." + addr + ">"
-                    print(data)
+                    pass
 
 
-                if data == b'rd':
+                if data == b'rd': # rd >>> read data - send back all needed player data
                     print("DATA REQUEST")
 
 
+players_count = 0
 
 while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        conn, addr = s.accept()
-        x = threading.Thread(target=testrig, args=(conn, addr))
-        x.start()
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen()
+            conn, addr = s.accept()
+            x = threading.Thread(target=testrig, args=(conn, addr))
+            x.start()
+            players_count += 1
+    except:
+        print("")
+        print("")
+        print("")
+        print('[ERROR]: Port Already In use / Already Binded.')
+
+        print("Is anyone else using this port / game? - Try using a different port number")
+        CC = input("Press Enter To Quit: ")
+        break
 
 
